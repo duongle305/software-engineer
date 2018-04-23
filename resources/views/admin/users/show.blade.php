2dcdf4f7660efcb0bc682905d4cd9a3fdc1a4222
Duong Le <duongrom.it.305@gmail.com>
2018-04-23 13:07:20 +0700

    update show

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -23,18 +23,14 @@
                                     <img src="{{ asset($user->photo) }}" alt="image" class="img-lg rounded-circle mb-2"/>
                                     <h4>{{ $user->first_name }} {{ $user->last_name }}</h4>
                                     <p class="text-muted">{{ $user->roles()->first()->display_name }}</p>
-                                    <p class="text-muted">{{ $user->email }}</p>
-                                    <button class="btn btn-info btn-sm mt-3 mb-4">Follow</button>
+                                    <a href="{{ route('users.edit',$user->id) }}" class="btn btn-warning btn-sm mt-3 mb-4">Sửa</a>
                                     <div class="border-top pt-3">
                                         <div class="row">
-                                            <div class="col-sm-4">
+                                            <div class="col-sm-12">
+                                                <p>Ngày sinh: {{ date('d-m-Y',strtotime($user->birthday)) }} </p>
                                                 <p>Giới tính: {{ ucwords(strtolower($user->sex)) }}</p>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                <p>Ngày sinh: {{ date('d-m-Y',strtotime($user->birthday)) }}</p>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                <p>SĐT: {{ $user->phone }}</p>
+                                                <p>Email: {{ $user->email }}</p>
+                                                <p>Địa chỉ thường trus</p>
                                             </div>
                                         </div>
                                     </div>
