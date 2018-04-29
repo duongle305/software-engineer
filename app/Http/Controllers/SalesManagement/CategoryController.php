4adabf325f3969fdfcf0eef9e2356c5f545c1e68
Duong Le <duongrom.it.305@gmail.com>
2018-04-29 14:41:00 +0700

    Xong suppliers

--- app/Http/Controllers/SalesManagement/CategoryController.php
+++ app/Http/Controllers/SalesManagement/CategoryController.php
@@ -69,7 +69,8 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-
+        $category = Category::find($id);
+        return view('admin.categories.edit')->withCategory($category);
     }
 
     /**
--- resources/views/admin/suppliers/show.blade.php
+++ resources/views/admin/suppliers/show.blade.php
@@ -7,7 +7,7 @@
 @endsection
 
 @section('wrapper')
-    <div class="row grid-margin">
+    <div class="row grid-margin" id="app">
         <div class="col-sm-12">
             <nav aria-label="breadcrumb" role="navigation">
                 <ol class="breadcrumb breadcrumb-custom">
@@ -22,33 +22,28 @@
                         <div class="col-sm-12 grid-margin stretch-card">
                             <div class="card text-center">
                                 <div class="card-body">
-                                    <h4 class="text-left">Suppeliter name</h4><br>
+                                    <h4 class="text-left mb-3">{{ $supplier->title  }}</h4>
                                     <div class="row">
-                                        <div class="col-sm-8">
+                                        <div class="col-xs-12 col-md-12">
                                             <div class="table-responsive">
                                                 <table class="table">
                                                     <tbody>
-                                                    <tr>
-                                                        <th class="text-left">Số điện thoại</th>
-                                                        <td class="text-left">0969363678</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th class="text-left">Email</th>
-                                                        <td class="text-left">alice@gmail.com</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th class="text-left">Địa chỉ</th>
-                                                        <td class="text-left">Quận 12, HCM</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th class="text-left">Ngày tạo</th>
-                                                        <td class="text-left">30/04/2018 12:00</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th class="text-left">Ngày cập nhật</th>
-                                                        <td class="text-left">30/04/2018 12:00</td>
-                                                    </tr>
-
+                                                        <tr>
+                                                            <th class="text-left">Số điện thoại: </th>
+                                                            <td class="text-left" >{{ $supplier->phone }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th class="text-left">Email: </th>
+                                                            <td class="text-left">{{ $supplier->email }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th class="text-left">Địa chỉ: </th>
+                                                            <td class="text-left">{{ $supplier->address }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th class="text-left">Ngày thêm: </th>
+                                                            <td class="text-left" >{{ $supplier->created_at->format('d-m-Y') }}</td>
+                                                        </tr>
                                                     </tbody>
                                                 </table>
                                             </div>
@@ -67,4 +62,12 @@
 
 @endsection
 @section('custom_js')
+    <script type="text/javascript">
+        let app = new Vue({
+            el: '#app',
+            data:{
+                isUpdateSupplier: false,
+            }
+        })
+    </script>
 @endsection
