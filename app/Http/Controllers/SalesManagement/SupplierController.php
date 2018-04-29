0787efd4fed218a240ca8373e3c8476c25bbb91d
Duong Le <duongrom.it.305@gmail.com>
2018-04-29 10:31:26 +0700

    update

--- app/Http/Controllers/SalesManagement/SupplierController.php
+++ app/Http/Controllers/SalesManagement/SupplierController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\SalesManagement;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class SupplierController extends Controller
 {
@@ -37,7 +38,25 @@ class SupplierController extends Controller
      */
     public function store(Request $request)
     {
-        $all = $request->only(['title','phone','email',''])
+        $all = $request->only(['title','phone','email','detail','ward','district','province']);
+        $validator = Validator::make($all,[
+            'title'=>'required|string',
+            'phone'=>'required|string:unique:suppliers,phone',
+            'email'=>'required|email|unique:suppliers,email',
+            'detail'=>'required',
+            'ward'=>'required',
+            'district'=>'required',
+            'province'=>'required',
+        ],[
+            'title.required'=>'Vui lòng nhập tên công nhà cung cấp',
+            'phone.required'=>'Vui lòng nhập số điện thoại nhà cung cấp.',
+            'email.required'=>'Vui lòng nhập địa chỉ email nhà cung cấp',
+            'email.email'=>'Địa chỉ email không đúng.',
+            'detail'=>'Vui lòng nhập số nhà, tên đường.',
+            'ward'=>'Vui lòng chọn phường, xã',
+            'district'=>'Vui lòng chọn quận, huyện',
+            'province'=>'Vui lòng chọn tỉnh, thành phố.',
+        ]);
     }
 
     /**
